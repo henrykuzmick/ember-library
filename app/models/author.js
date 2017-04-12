8fb93d48a669355321207cecd7937ed5b89d6fc2
Henry Kuzmick <henry.kuzmick@gmail.com>
2017-04-12 13:12:43 +0200

    list authors

--- app/models/author.js
+++ app/models/author.js
@@ -1,9 +1,11 @@
 import DS from 'ember-data';
 import Faker from 'faker';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   books: DS.hasMany('book'),
+  isNotValid: Ember.computed.empty('name'),
   randomize() {
     this.set('name', Faker.name.findName());
     return this;
